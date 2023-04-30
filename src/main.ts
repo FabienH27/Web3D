510eb46f7c0fc8bd358289560a9dcc35f16f1355
Fabien H <fhannon60@gmail.com>
2023-04-30 22:57:27 +0200

    Add first version with basic octopus body

--- src/main.ts
+++ src/main.ts
@@ -2,66 +2,78 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-import { camera, renderer } from './setup';
-
-const scene = new THREE.Scene();
+import { addControls, addEnvironment, camera, render, renderer, scene } from './setup';
 
 let bones: any = [];
 let betaAnim = 0;
 
-//Model
-const loader = new GLTFLoader().setPath('/models/gltf/');
-loader.load('Tentacule.glb', function (gltf) {
-    const cube = gltf.scene;
-    scene.add(cube);
+window.addEventListener('resize', onWindowResize);
 
-    let object = cube.getObjectByName("Bone");
-    bones.push(object);
-    let children = object?.children!;
+function init() {
 
-    while (!(object === undefined || children?.length == 0)) {
-        bones.push(children[0]);
-        children = children[0].children;
+    addEnvironment();
+
+    // Model
+    for (let index = 0; index < 8; index++) {
+        addModel(index);
     }
+    // addModel();
 
+    addControls();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
     animate();
-});
+}
 
+function addModel(index: number) {
+    const loader = new GLTFLoader().setPath('/models/gltf/');
+    loader.load('Tentacule.glb', function (gltf) {
+        const cube = gltf.scene;
+        const angle = index * Math.PI * 2 / 8;
+        const radius = 5;
+        cube.position.set(Math.cos(angle) * radius, cube.position.y, Math.sin(angle) * radius);
+        cube.rotateY(angle * -1);
+        scene.add(cube);
+
+        let object = cube.getObjectByName("Bone");
+        bones.push(object);
+        let children = object?.children!;
+
+        while (!(object === undefined || children?.length == 0)) {
+            bones.push(children[0]);
+            children = children[0].children;
+        }
+
+        animate();
+    });
+
+    loader.load('Octopus_head.glb', function(gltf){
+        const head = gltf.scene;
+        const scale = 1.4;
+        head.scale.set(scale, scale, scale);
+        scene.add(head);
+    })
+
+}
 
 function animate() {
     betaAnim += 0.01;
     requestAnimationFrame(animate);
     let changeposition = 0;
     bones.forEach((bone: BoneType) => {
-        changeposition += 10 * (Math.PI / 35)
-        bone.position.x = 0.3 * Math.sin((changeposition) + betaAnim);
+        changeposition += 10 * (Math.PI / 45)
+        bone.position.x = 0.2 * Math.sin((changeposition) + betaAnim);
         bone.rotation.z = 0.2 * Math.sin((changeposition) + betaAnim);
     });
     renderer.render(scene, camera);
 }
 
-function render() {
-    renderer.render(scene, camera);
-}
 
-function init() {
-    new THREE.TextureLoader()
-        .load('/textures/equirectangular/env.jpg', function (texture) {
-            texture.mapping = THREE.EquirectangularReflectionMapping;
-
-            scene.background = texture;
-            scene.environment = texture;
-
-            render();
-        });
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener('change', render); // use if there is no animation loop
-    controls.minDistance = 2;
-    controls.maxDistance = 30;
-    controls.target.set(0, 0, - 0.2);
-    controls.update();
-}
 
 init();
-animate();
--- src/setup.ts
+++ src/setup.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -9,10 +10,36 @@ renderer.toneMappingExposure = 1;
 renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
 
-const fov = 50;
+const fov = 60;
 const ratio = window.innerWidth / window.innerHeight;
 const znear = 0.25;
 const zfar = 100;
 
+export const scene = new THREE.Scene();
 export const camera = new THREE.PerspectiveCamera(fov, ratio, znear, zfar);
 camera.position.set(- 1.8, 0.6, 1000);
+
+
+export function render() {
+    renderer.render(scene, camera);
+}
+
+export function addControls(){
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render); // use if there is no animation loop
+    controls.minDistance = 2;
+    controls.maxDistance = 30;
+    controls.target.set(0, 0, - 0.2);
+    controls.update();
+}
+export function addEnvironment(){
+    new THREE.TextureLoader()
+    .load('/textures/equirectangular/env.jpg', function (texture) {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+
+        scene.background = texture;
+        scene.environment = texture;
+
+        render();
+    });
+}
